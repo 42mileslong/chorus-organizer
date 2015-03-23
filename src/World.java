08c2bff3965fe3bade73c7c011e6287b4699289d
bencw99 <ben.cohenwang@gmail.com>
2015-03-23 09:55:35 -0700

    Created world class

--- src/World.java
+++ src/World.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
+/**
+ * A class representing a world
+ * @author Benjamin Cohen-Wang
+ */
 public class World {
-
+	/** The collection of students of this world **/
+	ArrayList<Student> students;
+	
+	/**
+	 * Default constructor, initializes this world
+	 */
+	public World() {
+		students = new ArrayList<Student>();
+	}
+	
+	/**
+	 * Adds the given student to this world
+	 * 
+	 * @param student	the student to be added
+	 */
+	public void add (Student student) {
+		students.add(student);
+	}
+	
+	/**
+	 * Removes the given student from this world
+	 * 
+	 * @param student	the student to be removed
+	 */
+	public void remove (Student student) {
+		students.remove(student);
+	}
 }
