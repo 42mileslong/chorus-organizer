51cc8808bbab93adc2b8944a8b641125854c3823
bencw99 <ben.cohenwang@gmail.com>
2015-03-31 08:47:57 -0700

    Created color association functions

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 
 /**
  * The class representing a Student
@@ -8,15 +10,25 @@ public class Student {
      * The enum describing pitch
      */
     public static enum Pitch {
-    	SOPRANO,
-    	MEZZO_SOPRANO,
-    	CONTRALTO,
-    	COUNTERTENOR,
-    	TENOR,
-    	BARITONE,
-    	TREBLE,
-    	BASS,
-    	UNKNOWN
+    	SOPRANO(new Color(0, 0, 0)),
+    	MEZZO_SOPRANO(new Color(0, 0, 0)),
+    	CONTRALTO(new Color(0, 0, 0)),
+    	COUNTERTENOR(new Color(0, 0, 0)),
+    	TENOR(new Color(0, 0, 0)),
+    	BARITONE(new Color(0, 0, 0)),
+    	TREBLE(new Color(0, 0, 0)),
+    	BASS(new Color(0, 0, 0)),
+    	UNKNOWN(new Color(0, 0, 0));
+    	
+    	Color color;
+    	
+    	private Pitch(Color color) {
+    		this.color = color;
+    	}
+    	
+    	public Color getColor() {
+    		return color;
+    	}
     }
 	
     /**
@@ -26,6 +38,77 @@ public class Student {
     	MALE,
     	FEMALE,
     	UNKNOWN
+    }
+    
+    /**
+     * Returns the color corresponding to the given grade
+     * 
+     * @param grade	the grade to be converted
+     * @return the color of this grade
+     */
+    public static Color getGradeColor(int grade) {
+    	switch(grade) {
+    	case 9:
+    		return new Color(0, 0, 0);
+    	case 10:
+    		return new Color(0, 0, 0);
+    	case 11:
+    		return new Color(0, 0, 0);
+    	case 12:
+    		return new Color(0, 0, 0);
+    	default:
+    		return new Color(0, 0, 0);
+    	}
+    }
+    
+    /**
+     * Returns the color corresponding to the given skill level
+     * 
+     * @param skill	the skill to be converted
+     * @return the color of this skill
+     */
+    public static Color getSkillColor(int skill) {
+    	switch(skill) {
+    	case 1:
+    		return new Color(0, 0, 0);
+    	case 2:
+    		return new Color(0, 0, 0);
+    	case 3:
+    		return new Color(0, 0, 0);
+    	case 4:
+    		return new Color(0, 0, 0);
+    	case 5:
+    		return new Color(0, 0, 0);
+    	case 6:
+    		return new Color(0, 0, 0);
+    	case 7:
+    		return new Color(0, 0, 0);
+    	case 8:
+    		return new Color(0, 0, 0);
+    	case 9:
+    		return new Color(0, 0, 0);
+    	case 10:
+    		return new Color(0, 0, 0);
+    	default:
+    		return new Color(0, 0, 0);
+    	}
+    }
+    
+    /**
+     * Returns the color corresponding to the given skill level and pitch
+     * 
+     * @param skill	the skill to be converted
+     * @param pitch	the color associated with the pitch
+     * @return the color of this skill and pitch
+     */
+    public static Color getColor(Color pitch, int skill) {
+    	int colorAvg = (pitch.getRed() + pitch.getGreen() + pitch.getBlue())/3;
+    	
+    	int newRed = (pitch.getRed()*skill + colorAvg)/(skill + 1);
+    	int newGreen = (pitch.getGreen()*skill + colorAvg)/(skill + 1);
+    	int newBlue = (pitch.getBlue()*skill + colorAvg)/(skill + 1);
+    	
+    	return new Color(newRed, newGreen, newBlue);
     }
     
 	/** The name of this student **/
@@ -42,6 +125,9 @@ public class Student {
  
     /** The grade of this student **/
     private int grade;
+    
+    /** The skill of this choir member **/
+    private int skill;
     
     /** The default constructor of a student, sets fields to unknown **/
     public Student() {
@@ -61,6 +147,13 @@ public class Student {
     	setGender(gender);
     	setGrade(grade);
     }
+    
+    /**
+	 * @return the skill
+	 */
+	public int getSkill() {
+		return skill;
+	}
     
     /**
 	 * @return the name
@@ -95,6 +188,13 @@ public class Student {
 	 */
 	public int getGrade() {
 		return grade;
+	}
+	
+	/**
+	 * @param name the skill to set
+	 */
+	public void setSkill(int skill) {
+		this.skill = skill;
 	}
 
 	/**
