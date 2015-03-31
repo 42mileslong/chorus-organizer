c3c39fa47fe33ff43f6c5b490af5c547e43b0283
Rafi Long <rafilong42@gmail.com>
2015-03-31 09:08:22 -0700

    Created Main class with display initialization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
- *
+ * Main method
+ * Creates display object and frame
+ * Adds display to frame
  */
 public class Main {
+    private static Display display;
 
+    private static Dimension screenSize;
+
+    /**
+     * Creates a frame and adds the display to the frame
+     * @param args
+     */
+    public static void main(String args[]) {
+        initDisplay();
+    }
+
+    public static void initDisplay() {
+        // finds the available size of the screen
+        initScreenSize();
+
+        // creates a new display object
+        display = new Display();
+
+        // initializes the frame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
+        frame.setVisible(true);
+        frame.add(display);
+    }
+
+    public static void initScreenSize() {
+        screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
+    }
+
+    public static Display getDisplay() {
+        return display;
+    }
+
+    public static Dimension getScreenSize() {
+        return screenSize;
+    }
 }
