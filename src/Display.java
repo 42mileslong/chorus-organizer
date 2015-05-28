5c5e2072bf6964d1c55a9462a838cf04641fef8e
bencw99 <ben.cohenwang@gmail.com>
2015-05-28 09:44:52 -0700

    Started adding menu bar

--- src/Display.java
+++ src/Display.java
@@ -1,16 +1,33 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * Display class
  */
-public class Display extends JComponent {
-
+public class Display extends JPanel implements MouseListener {
+	/** The menu bar of this display panel **/
+	private JMenuBar menuBar;
+	
+	/** The file menu of this display panel **/
+	private JMenu file;
+	private JMenu newItem;
+	
     /**
      * No need for content in constructor: display automatically paints on create
      */
     public Display() {
-
+    	menuBar = new JMenuBar();
+    	
+    	file = new JMenu("File");
+    	file.setMnemonic(KeyEvent.VK_A);
+    	file.getAccessibleContext().setAccessibleDescription("File");
+    	
+    	menuBar.add(file);
+    	this.add(menuBar);
     }
 
     /**
@@ -23,4 +40,39 @@ public class Display extends JComponent {
     public void paint(Graphics g) {
         Main.getWorld().draw(g);
     }
+
+	@Override
+	public void mouseClicked(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
